3cfd9911ac389a94ce565afed1efb6f787ac122e
Crispin Read <crispin.read@gmail.com>
2018-11-29 19:43:55 +0000

    start vid at 0 seconds

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 //video id from youtube
 $('document').ready(function() {
-	var options = { videoId: 'agrHTsFUXBc', start: 3 };
+	var options = { videoId: 'agrHTsFUXBc', start: 0 };
 	$('#wrapper').tubular(options);
 });
